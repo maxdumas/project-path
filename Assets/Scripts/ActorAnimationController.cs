a97352c800befef260d7eaca31f758d369c5900a
Max Dumas <maltor124@gmail.com>
2014-09-22 23:46:55 -0400

    Rebuilt animation system to use Animation Triggers.

---
+++ Assets/Scripts/ActorAnimationController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActorAnimationController : MonoBehaviour
+{
+    public Actor TargetActor;
+    public CombatWindow TargetCombatWindow;
+
+    public void AnimAttack()
+    {
+        TargetCombatWindow.OnAnimAttack(TargetActor);
+    }
+
+    public void AnimBeginDefense()
+    {
+        TargetCombatWindow.OnAnimDefenseBegin(TargetActor);
+    }
+
+    public void AnimEndDefense()
+    {
+        TargetCombatWindow.OnAnimDefenseEnd(TargetActor);
+    }
+
+    public void AnimBeginIdle()
+    {
+        
+    }
+
+    public void AnimEndIdle()
+    {
+        
+    }
+}
